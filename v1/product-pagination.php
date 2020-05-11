245bb593ddffce1769e4e59416282e5be757df0c
evankhan0123 <evankhan1234@gmail.com>
2020-05-10 17:13:26 -0700

    ok

---
+++ v1/product-pagination.php
@@ -0,0 +1,81 @@
+<?php
+
+ini_set("display_errors", 1);
+
+require '../vendor/autoload.php';
+use \Firebase\JWT\JWT;
+
+//include headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Content-type: application/json; charset=utf-8");
+
+// including files
+include_once("../config/database.php");
+include_once("../classes/Users.php");
+
+//objects
+$db = new Database();
+
+$connection = $db->connect();
+
+$user_obj = new Users($connection);
+
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+
+    // body
+    $data = json_decode(file_get_contents("php://input"));
+
+
+    $headers = getallheaders();
+
+
+
+    try{
+
+
+        $jwt = $headers["Authorization"];
+
+        $secret_key = "owt125";
+
+        $decoded_data = JWT::decode($jwt, $secret_key, array('HS512'));
+        $user_obj->user_id = $decoded_data->data->Id;
+        $user_obj->limit = $data->limit;
+        $user_obj->page = $data->page;
+
+        $products=$user_obj->getProduct();
+
+        if($products){
+
+            http_response_code(200); // ok
+            echo json_encode(array(
+                "status" => 200,
+                "success" => true,
+                "data" => $products,
+                "message" => "Products Found"
+            ));
+        }else{
+
+            http_response_code(500); //server error
+            echo json_encode(array(
+
+                "status" => 200,
+                "success" => true,
+                "data" => $products,
+                "message" => "No Products Found"
+            ));
+        }
+    }catch(Exception $ex){
+
+        http_response_code(500); //server error
+        echo json_encode(array(
+            "status" => 501,
+            "success" => false,
+            "message" => $ex->getMessage()
+        ));
+    }
+
+
+}
+
+?>
