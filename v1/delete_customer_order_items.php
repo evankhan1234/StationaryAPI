ed2a4eea773435fe3c034d47e2f45bd7b0f36263
evankhan0123 <evankhan1234@gmail.com>
2020-05-15 16:59:00 -0700

    ok

---
+++ v1/delete_customer_order_items.php
@@ -0,0 +1,82 @@
+<?php
+
+ini_set("display_errors", 1);
+
+require '../vendor/autoload.php';
+use \Firebase\JWT\JWT;
+
+//include headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Content-type: application/json; charset=utf-8");
+
+// including files
+include_once("../config/database.php");
+include_once("../classes/Users.php");
+
+//objects
+$db = new Database();
+
+$connection = $db->connect();
+
+$user_obj = new Users($connection);
+
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+
+    // body
+    $data = json_decode(file_get_contents("php://input"));
+
+
+    $headers = getallheaders();
+    $jwt = $headers["Authorization"];
+
+    $secret_key = "owt125";
+
+    $decoded_data = JWT::decode($jwt, $secret_key, array('HS512'));
+    if(!empty($decoded_data->data->Id)){
+
+        try{
+
+
+            $user_obj->orders_details_shop_id = $decoded_data->data->Id;
+            $user_obj->orders_details_id = $data->Id;
+
+            if($user_obj->delete_customer_order_items()){
+
+                http_response_code(200); // ok
+                echo json_encode(array(
+                    "status" => 200,
+                    "success" => true,
+                    "message" => "Deleted Item SuccessFull"
+                ));
+            }else{
+
+                http_response_code(500); //server error
+                echo json_encode(array(
+
+                    "status" => 500,
+                    "success" => false,
+                    "message" => "Failed to Deleted Deleted Item"
+                ));
+            }
+        }catch(Exception $ex){
+
+            http_response_code(500); //server error
+            echo json_encode(array(
+                "status" => 501,
+                "success" => false,
+                "message" => $ex->getMessage()
+            ));
+        }
+    }else{
+
+        http_response_code(404); // not found
+        echo json_encode(array(
+            "status" => 503,
+            "success" => false,
+            "message" => "All data needed"
+        ));
+    }
+}
+
+?>
