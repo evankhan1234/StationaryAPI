26f5560689f2b33bad5d3dd4088455be8fcd77fb
evankhan0123 <evankhan1234@gmail.com>
2020-07-17 22:03:22 -0700

    ok

---
+++ v1/get-shop-by-latitude.php
@@ -0,0 +1,70 @@
+<?php
+
+ini_set("display_errors", 1);
+
+require '../vendor/autoload.php';
+use \Firebase\JWT\JWT;
+
+//include headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Content-type: application/json; charset=utf-8");
+
+// including files
+include_once("../config/database.php");
+include_once("../classes/Users.php");
+
+//objects
+$db = new Database();
+
+$connection = $db->connect();
+
+$user_obj = new Users($connection);
+
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+
+    // body
+    $data = json_decode(file_get_contents("php://input"));
+
+    //  $user_obj->user_email = $data->Email;
+    $headers = getallheaders();
+
+
+
+
+
+    try{
+
+
+        $jwt = $headers["Authorization"];
+
+        $secret_key = "owt125";
+
+        $decoded_data = JWT::decode($jwt, $secret_key, array('HS512'));
+        $user_obj->shop_user_id = $data->ShopId;
+
+        $users=$user_obj->getShopByIdLatitude();
+
+
+        http_response_code(200); // ok
+        echo json_encode(array(
+            "status" => 200,
+            "success" => true,
+            "data" => $users,
+            "message" => "Shop Found"
+        ));
+
+
+    }catch(Exception $ex){
+
+        http_response_code(500); //server error
+        echo json_encode(array(
+            "status" => 501,
+            "success" => false,
+            "message" => $ex->getMessage()
+        ));
+    }
+
+}
+
+?>
